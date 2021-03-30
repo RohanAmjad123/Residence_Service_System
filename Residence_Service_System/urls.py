9c41b7ec661a07cfc095bd9db7cdb065dac2be0f
Rohan Amjad <rohanamjad123@gmail.com>
2021-03-29 21:51:11 -0600

    added api/ to url represent api app

--- Residence_Service_System/urls.py
+++ Residence_Service_System/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
